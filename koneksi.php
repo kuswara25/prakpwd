dd0f5c7d713e2a7f381c956b73e481e1b977f308
kuswara25 <92174697+kuswara25@users.noreply.github.com>
2021-12-17 21:35:39 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$ host = "host lokal" ;
-$ namapengguna = "akar" ;
-$ sandi = "" ;
-$ Databasename = "akedemik1" ;
-$ con =@ mysqli_connect ( $ host , $ username , $ password , $ databasename );
-jika (! $ kon )
-{
-    gema  "Kesalahan: " . mysqli_connect_error ();
-    keluar ();
-}
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $databasename = "akedemik1";
+    $con = @mysqli_connect($host,$username,$password,$databasename);
+
+    if(!$con){
+        echo "Error: ". mysqli_connect_error();
+        exit();
+    }
 ?>
